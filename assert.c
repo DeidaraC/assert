b99db78ad5da4889ee3c66ed3b94e8e6429370d1
JIANG Di <jiang-di@outlook.com>
2015-10-12 23:52:14 +0800

    :art: format code

--- assert.c
+++ assert.c
@@ -3,10 +3,8 @@
 #include "assert.h"
 
 void __assert(const char *func, const char *file, int line, const char *expr) {
-  fprintf(stderr,
-    "Assertion failed: (%s), function %s, file %s, line %d.\n",
-    expr, func, file, line
-  );
+  fprintf(stderr, "Assertion failed: (%s), function %s, file %s, line %d.\n",
+          expr, func, file, line);
 
   abort();
 }
--- assert.h
+++ assert.h
@@ -1,8 +1,10 @@
 #undef assert
 
 #ifdef NDEBUG
-  #define assert(e) ((void)0)
+#define assert(e) ((void)0)
 #else
-  void __assert(const char *func, const char *file, int line, const char *expr);
-  #define assert(e) ((e) ? ((void)0) : (__assert(__func__, __FILE__, __LINE__, #e)))
+void __assert(const char *func, const char *file, int line, const char *expr);
+
+#define assert(e)                                                              \
+  ((e) ? ((void)0) : (__assert(__func__, __FILE__, __LINE__, #e)))
 #endif
